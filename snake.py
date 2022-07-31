cfc2eed4c8cb5f6de3658aef2da14a674b1861be
TheRightHandOfTheDevil <92685009+TheRightHandOfTheDevil@users.noreply.github.com>
2022-07-31 15:26:29 +0200

    Update snake.py

--- snake.py
+++ snake.py
@@ -1,6 +1,9 @@
 import pygame
 from random import randint
 
+pygame.init()
+pygame.font.init()
+
 #screen dimensions
 WIDTH = 700
 HEIGHT = 500
@@ -28,6 +31,11 @@ VERTICAL_OFFSET = 50
 OFFSET_COLOR = BLACK
 COLUMNS = (WIDTH - 2 * SIDE_OFFSET) // TILE_SIZE
 ROWS = (HEIGHT - 2 * VERTICAL_OFFSET) // TILE_SIZE
+
+def centeredx_text(surface, y, font, color, string, height):
+    format = pygame.font.SysFont(font, height)
+    text = format.render(string, True, color)
+    surface.blit(text, ((surface.get_width() -text.get_width())/2, y))
 
 def draw_bg():
     #draw the screen
@@ -70,13 +78,14 @@ class Snake:
             body_rect = pygame.Rect(self.x, self.y + TILE_SIZE*(i+1), TILE_SIZE, TILE_SIZE)
             self.body_rects.append(body_rect)
 
+    #draw every piece of the snake
     def draw(self):
-
         for rect in self.body_rects:
             pygame.draw.rect(screen, self.body_color, rect)
         pygame.draw.rect(screen, self.head_color, self.head_rect)
         pygame.draw.rect(screen, WHITE, self.head_rect, 1)
 
+    #changing the direction
     def move(self):
         self.keys = pygame.key.get_pressed()
         if self.x_vel != 0 or self.y_vel != 0:
@@ -95,6 +104,7 @@ class Snake:
             self.y_vel = 1
             self.x_vel = 0
 
+        #every piece of the body gets the position of the previous one
         if pygame.time.get_ticks() - self.cooldown >= self.wait and self.moving and self.alive:
             if len(self.body_rects) > 1:
                 self.body_rects[-1:0:-1] = self.body_rects[-2::-1]
@@ -111,6 +121,7 @@ class Snake:
 
         if self.head_rect.colliderect(self.target):
 
+            #check where to place the new body rectangle
             if len(self.body_rects) == 0:
                 self.new_rect = pygame.Rect(self.head_rect[0], self.head_rect[1], TILE_SIZE, TILE_SIZE)
                 if self.x_vel == -1:
@@ -122,42 +133,34 @@ class Snake:
                 elif self.y_vel == 1:
                     self.new_rect.y -= TILE_SIZE
 
-            elif len(self.body_rects) == 1:
+            else:
                 self.new_rect = self.body_rects[-1]
 
-                if self.body_rects[-1].x == self.head_rect.x + TILE_SIZE:
-                    self.new_rect.x += TILE_SIZE
-                elif self.body_rects[-1].x == self.head_rect.x - TILE_SIZE:
-                    self.new_rect.x -= TILE_SIZE
-                if self.body_rects[-1].y == self.head_rect.y + TILE_SIZE:
-                    self.new_rect.y += TILE_SIZE
-                elif self.body_rects[-1].y == self.head_rect.y - TILE_SIZE:
-                    self.new_rect.y -= TILE_SIZE
-
-            elif len(self.body_rects) >= 2:
-                self.new_rect = self.body_rects[-1]
-
-                if self.body_rects[-1].x == self.body_rects[-2].x - TILE_SIZE:
-                    self.new_rect.x += TILE_SIZE
-                elif self.body_rects[-1].x == self.body_rects[-2].x + TILE_SIZE:
-                    self.new_rect.x -= TILE_SIZE
-                if self.body_rects[-1].y == self.body_rects[-2].y - TILE_SIZE:
-                    self.new_rect.y += TILE_SIZE
-                elif self.body_rects[-1].y == self.body_rects[-2].y + TILE_SIZE:
-                    self.new_rect.y -= TILE_SIZE
+                if len(self.body_rects) >= 2:
+                    if self.body_rects[-1].x == self.body_rects[-2].x - TILE_SIZE:
+                        self.new_rect.x += TILE_SIZE
+                    elif self.body_rects[-1].x == self.body_rects[-2].x + TILE_SIZE:
+                        self.new_rect.x -= TILE_SIZE
+                    if self.body_rects[-1].y == self.body_rects[-2].y - TILE_SIZE:
+                        self.new_rect.y += TILE_SIZE
+                    elif self.body_rects[-1].y == self.body_rects[-2].y + TILE_SIZE:
+                        self.new_rect.y -= TILE_SIZE
 
             self.body_rects.append(self.new_rect)
 
+    #check the collison betweeen the head and the border
     def border_collision(self):
         if self.head_rect.right > WIDTH - SIDE_OFFSET or self.head_rect.left < SIDE_OFFSET \
             or self.head_rect.bottom > HEIGHT - VERTICAL_OFFSET or self.head_rect.top < VERTICAL_OFFSET:
             self.alive = False
 
+    #check collision between the head and the body
     def body_collision(self):
         for body_rect in self.body_rects:
             if self.head_rect == body_rect:
                 self.alive = False
 
+#food class
 class Food:
     def __init__(self, color, wait, target):
         self.color = color
@@ -170,6 +173,7 @@ class Food:
 
         self.rect = pygame.Rect(self.x*TILE_SIZE + SIDE_OFFSET, self.y*TILE_SIZE + VERTICAL_OFFSET, TILE_SIZE, TILE_SIZE)
 
+    #if the food is eaten, generate another one
     def eat(self):
         if self.target.colliderect(self.rect):
             self.x = randint(0, COLUMNS - 1)
@@ -179,10 +183,12 @@ class Food:
             self.alive = False
             self.cooldown = pygame.time.get_ticks()
 
+    #if enough time is passed since the last food is eaten, create another one
     def spawn(self):
         if self.alive == False and pygame.time.get_ticks() - self.cooldown >= self.wait:
             self.alive = True
 
+    #draw the food
     def draw(self):
         if self.alive:
             pygame.draw.rect(screen, self.color, self.rect)
@@ -191,35 +197,64 @@ class Food:
 def main():
 
     run = True
+    gameover = False
 
     #snake instance
-    snake = Snake(1, RED, BLACK, SIDE_OFFSET + 10*TILE_SIZE, VERTICAL_OFFSET + 5*TILE_SIZE, 150)
+    snake = Snake(4, RED, BLACK, SIDE_OFFSET + 10*TILE_SIZE, VERTICAL_OFFSET + 5*TILE_SIZE, 150)
 
     #food instance
     food = Food(BLUE, 1000, snake.head_rect)
 
     while run:
 
+        #use the choosen framerate
         clock.tick(FPS)
 
-        #event handler
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                run = False
+        # on-screen variables
+        body_lenght = len(snake.body_rects)
+        food_eaten = len(snake.body_rects) - snake.start_body_lenght
+        
+        if gameover == False:
 
-        draw_bg()
-        draw_grid()
-        snake.draw()
-        snake.move()
-        snake.eat(food)
-        snake.border_collision()
-        snake.body_collision()
+            #draw all the game window
+            draw_bg()
+            draw_grid()
 
-        if snake.alive:
+            #snake
+            snake.draw()
+            snake.move()
+            snake.eat(food)
+            snake.border_collision()
+            snake.body_collision()
+
+            #food
             food.spawn()
             food.eat()
             food.draw()
 
+            centeredx_text(screen, 15, "rockwellnova", WHITE, "BODY LENGHT: " + str(body_lenght), 20)
+            centeredx_text(screen, HEIGHT - 40, "rockwellnova", WHITE, "FOOD EATEN: " + str(food_eaten), 20)
+
+            if snake.alive == False:
+                gameover = True
+
+        #gameover settings
+        if gameover:
+            #screen.fill(BLACK)
+            centeredx_text(screen, HEIGHT // 2 - 150, "rockwellnova", RED, "YOU LOST", 50)
+            centeredx_text(screen, HEIGHT // 2 - 50, "rockwellnova", RED, "PRESS SPACE TO PLAY AGAIN", 30)
+
+            if pygame.key.get_pressed()[pygame.K_SPACE]:
+                gameover = False
+                snake = Snake(4, RED, BLACK, SIDE_OFFSET + 10 * TILE_SIZE, VERTICAL_OFFSET + 5 * TILE_SIZE, 150)
+                food = Food(BLUE, 1000, snake.head_rect)
+
+        # event handler
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+        #update the screen
         pygame.display.update()
 
 if __name__ == "__main__":
